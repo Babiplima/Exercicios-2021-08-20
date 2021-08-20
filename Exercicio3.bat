2de376447b35d327ce44d24847de9a7b4ce2c578
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-20 16:07:20 -0300

    Second commit Paises

--- Exercicio3.bat
+++ Exercicio3.bat
@@ -1,1 +1,3 @@
 md "Todos os Paises"
+cd "Todos os Paises"
+md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"
