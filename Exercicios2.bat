fbc033070d518b66c9f568b55a27f510d7a21a19
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-20 15:48:19 -0300

    Third commit Estados

--- Exercicios2.bat
+++ Exercicios2.bat
@@ -3,3 +3,6 @@ cd "Todos os estados"
 md "Centro Oeste" "Nordeste" "Norte" "Sul" "Sudeste"
 cd "Centro Oeste"
 md "Mato Grosso" "Goias" "Mato Grosso do Sul" "Distrito Federal" "
+cd ..
+cd "Nordeste"
+md "Alagoas" "Bahia" "Ceara" "Maranhao" "Paraiba" "Pernanbuco" "Piaui" "Rio Grande do Norte" "Sergipe"
